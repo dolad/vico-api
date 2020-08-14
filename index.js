720d3039ca2498f101dd1acb90af90691e607940
Akande David <adavidoladele@gmail.com>
2020-08-14 10:56:08 +0100

    deploying

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const port = process.env.PORT || 8000;
 
 //__________ listening form the port________
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  console.log(port);
+}
 
 app.listen(port, () => {
   console.log("app is running on port : " + port);
